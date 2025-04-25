259a6ef33c7c7a0f1a4daef2fa2bac95bc4a52f0
Ubuntu <mohammedaz@linuxvim-1.xg3gl4m2hetujmbronfeuurk1d.px.internal.cloudapp.net>
2025-04-25 12:16:22 +0000

    ver-9

--- deploy-to-aks.sh
+++ deploy-to-aks.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 RESOURCE_GROUP="rg-devopsproj2"
-AKS_CLUSTER="my-aks-cluster"
+AKS_CLUSTER="aksvm-devo"
+#"my-aks-cluster"
 
 echo "⏳ Getting AKS credentials..."
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER
 
 echo "🚀 Applying Kubernetes deployment..."
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f k8/deployment.yaml
+kubectl apply -f k8/service.yaml
 
 echo "✅ Deployment to AKS completed!"
 
